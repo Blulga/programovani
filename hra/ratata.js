3ee3ded0f53e736facb31ffc205de5d0eb0eb7a9
x <marek.cz2010@gmail.com>
2023-12-07 17:03:36 +0100

    bullets

---
+++ hra/ratata.js
@@ -0,0 +1,18 @@
+class Bullet {
+    constructor (pos) {
+        this.pos = pos;
+
+        var mouse = createVector(mouseX, mouseY);
+        this.dir = p5.Vector.sub(mouse, this.pos);
+        this.dir.setMag(5);
+    }
+
+    update() {
+        this.pos.add(this.dir);
+    }
+
+    draw() {
+        circle(this.pos.x, this.pos.y, 5);
+    }
+
+}
