6135de403e814a2398f8e5f0a170b85cf33a708c
Marek <marek.cz2010@gmail.com>
2024-03-21 15:45:30 +0100

    neco

--- engine3/src/main.js
+++ engine3/src/main.js
@@ -1,6 +1,41 @@
 
+class Player extends Component {
+
+    start() {
+        this.rb = this.getComponent(DynamicBoxCollider);
+
+        this.rb.onCollisionEnter = (col) =
+        
+    }
+
+    update() {
+        if(Input.keyJustPressed(' ')) {
+            this.rb.vel.y = -10  
+        }
+
+        if(Input.keyPressed('D')) {
+            this.rb.vel.x = 10  
+        }
+        else if(Input.keyPressed('d')) {
+            this.rb.vel.x = 5  
+        }
+        else if(Input.keyPressed('A')) {
+            this.rb.vel.x = -10  
+        }
+        else if(Input.keyPressed('a')) {
+            this.rb.vel.x = -5
+        }
+        else this.rb.vel.x = 0
+       
+        
+    }
+}
+
+
+
+
 function setup() {
-    size(800, 600);
+    size(1400, 700);
 
     new GameObject(width / 2, height - 5)
     .addComponent(new RectRenderer(width, 10, () => {
@@ -8,6 +43,15 @@ function setup() {
         fill(0, 255, 0);
     }))
     .addComponent(new BoxCollider(width, 10))
+
+    new GameObject(width / 2, height / 2)
+    .addComponent(new Player())
+    .addComponent(new RectRenderer(100, 50))
+    .addComponent(new DynamicBoxCollider(100, 50))
+
+    new GameObject(width / 2, height / 2)
+    .addComponent(new RectRenderer(100, 50))
+    .addComponent(new BoxCollider(100, 50))
 }
 
 function draw() {
